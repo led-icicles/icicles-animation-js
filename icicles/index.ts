7cd982c1ec14feec1c63d294146325ee91814870
Kamil Klyta <kamil.klyta@htdevelopers.com>
2020-12-26 14:22:10 +0100

    build mov

--- icicles/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./frames/additive_frame";
-export * from "./frames/visual_frame";
-export * from "./frames/frame";
-export * from "./animation";
-export * from "./icicles";
---
+++ index.ts
@@ -0,0 +1,5 @@
+export * from "./icicles/frames/additive_frame";
+export * from "./icicles/frames/visual_frame";
+export * from "./icicles/frames/frame";
+export * from "./icicles/animation";
+export * from "./icicles/icicles";
