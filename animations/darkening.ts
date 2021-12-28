72f7710987b6d6b29a68e8e1f9540d3c3504bb7e
gonuit <kamilklyta341@gmail.com>
2021-12-28 16:35:04 +0100

    Add darkening animation

--- animations/darkening.ts
+++ animations/darkening.ts
@@ -4,31 +4,38 @@ const compile = async () => {
   const xCount = 20;
   const yCount = 30;
   const animation = new Animation({
-    name: "Dimm-violet",
+    name: "Darkening",
     optimize: true,
     versionNumber: 1,
     xCount,
     useRgb565: false,
     yCount,
     radioPanelsCount: 2,
-    loopsCount: 0,
+    loopsCount: 2,
   });
   const icicles = new Icicles(animation);
 
   const dimm = (color: Color) => {
     let i = 0;
-    for (let i = -Math.PI * 0.5; i <= Math.PI * 1.5; i += 0.1) {
+    for (let i = -Math.PI * 0.5; i <= Math.PI * 1.5; i += 0.05) {
       const val = (Math.sin(i) + 1) * 0.5;
-      console.log(val);
       const target = Color.linearBlend(Colors.black, color, val);
       icicles.setAllPixelsColor(target);
       icicles.setRadioPanelColor(0, target);
-      icicles.show(new Duration({ milliseconds: 20 }));
+      icicles.show(new Duration({ milliseconds: 60 }));
     }
   };
+  dimm(Colors.red);
+  dimm(Colors.lightBlue);
+  dimm(Colors.orange);
   dimm(Colors.violet);
+  dimm(Colors.lawnGreen);
+  dimm(Colors.magenta);
+  dimm(Colors.yellow);
+  dimm(Colors.blue);
+  dimm(Colors.green);
 
-  await animation.toFile(`./compiled/dimm-violet.anim`);
+  await animation.toFile(`./compiled/darkening.anim`);
 };
 
 compile();
